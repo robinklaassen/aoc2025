c640c7ad081b3ce26e7acb9546fb7dad6708ebb3
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2025-12-31 08:56:54 +0100

    solved day 5

---
+++ src/day05.rs
@@ -0,0 +1,70 @@
+fn prep_data(lines: &Vec<String>) -> (Vec<(i64, i64)>, Vec<i64>) {
+    let mut is_range_part = true;
+    let mut ranges: Vec<(i64, i64)> = Vec::new();
+    let mut ids: Vec<i64> = Vec::new();
+    for line in lines {
+        if line.is_empty() {
+            is_range_part = false;
+            continue;
+        }
+
+        if is_range_part {
+            let parts: Vec<&str> = line.split('-').collect();
+            let start: i64 = parts[0].parse().unwrap();
+            let end: i64 = parts[1].parse().unwrap();
+            ranges.push((start, end));
+        } else {
+            ids.push(line.parse().unwrap());
+        }
+    }
+    (ranges, ids)
+}
+
+fn part1(lines: &Vec<String>) -> i32 {
+    let mut answer = 0;
+    let (ranges, ids) = prep_data(lines);
+
+    // check each id against the ranges
+    ids.iter()
+        .filter(|&&id| ranges.iter().any(|&(start, end)| id >= start && id <= end))
+        .count() as i32
+}
+
+fn part2(lines: &Vec<String>) -> i64 {
+    let mut answer = 0;
+    let (mut ranges, _) = prep_data(lines);
+
+    // sort them on start ascending
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    // merge overlapping ranges by comparing current end to next start
+    let (mut start, mut end) = ranges[0];
+    for i in 1..ranges.len() {
+        let (next_start, next_end) = ranges[i];
+        if next_start <= end {
+            // ranges overlap, extend the end if needed
+            end = end.max(next_end);
+        } else {
+            // no overlap, add the current range to the answer and start a new one
+            answer += end - start + 1;
+            start = next_start;
+            end = next_end;
+        }
+    }
+
+    // add the last range
+    answer += end - start + 1;
+
+    answer
+}
+
+pub fn main() {
+    let test_lines = crate::utils::read_lines("input_test/day05.txt");
+    let input_lines = crate::utils::read_lines("input/day05.txt");
+
+    assert_eq!(part1(&test_lines), 3);
+    println!("Day 4 part 1 answer: {}", part1(&input_lines));
+
+    assert_eq!(part2(&test_lines), 14);
+    println!("Day 4 part 2 answer: {}", part2(&input_lines));
+}
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 mod utils;
 
 fn main() {
     // day01::main();
     // day02::main();
     // day03::main();
-    day04::main();
+    // day04::main();
+    day05::main();
 }
